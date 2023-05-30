f8662d70b3e921316cf3398e21e8d6a29b7f0231
GammaDC <yuran2000i@mail.ru>
2023-05-30 21:31:39 +0300

    Lab1

---
+++ p0031firstproject/src/main/java/com/emelianov/p0031firstproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.emelianov.p0031firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
